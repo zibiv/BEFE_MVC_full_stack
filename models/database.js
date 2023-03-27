ef1e99bf6d5f2e6f0d6312f1c8676c873d250a36
GenIl <topgear09@yandex.ru>
2023-03-27 11:30:18 +0300

    использование env переменных при настройке пула клиентов для БД

--- models/database.js
+++ models/database.js
@@ -1,4 +1,6 @@
 const { Pool } = require('pg');
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const pool = new Pool({
   user: process.env.DB_USER,
