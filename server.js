6a3f6845bae24f8d6210201095f727f41dad545e
GenIl <topgear09@yandex.ru>
2023-03-28 18:16:11 +0300

    Добавлен и настроен cors для работы с запросами с FE

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 require('dotenv').config();
 const expenseRoutes = require('./routes/expense');
+const cors = require('cors');
 
 // Running express server
 const app = express();
 const port = process.env.PORT || 8000;
-
+app.use(cors({
+  origin: "http://127.0.0.1:5173"
+}));
 // route middlewares
 app.use('/api', expenseRoutes);
 
